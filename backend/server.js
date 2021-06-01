270d8c290fb83f1a907fcad11401a58e910a2e29
lzg187ljh <54044136+lzg187ljh@users.noreply.github.com>
2021-05-31 23:44:31 -0400

    debug error

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,15 @@ app.use((err,req,res,next) =>{
 app.get('/api/config/paypal',(req,res)=>{
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-app.get('/',(req,res)=>{
-    res.send('Server is Ready');
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname,'/frontend/build')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/frontend/build/index.html'));
 });
+// app.get('/',(req,res)=>{
+//     res.send('Server is Ready');
+// });
 
 // app.get('/api/orders/mine',
 //     expressAsyncHandler(async(req,res) => {
